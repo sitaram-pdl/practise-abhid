5272e02d167ebd78ea8d27f87382ae89711ad33c
abidh_here <merosagramatha047@gmail.com>
2025-07-28 13:38:08 +0545

    feat: updates required eslint rules

--- dashboard/eslint.config.js
+++ dashboard/eslint.config.js
@@ -1,96 +1,23 @@
-// import js from '@eslint/js'
-// import globals from 'globals'
-// import reactHooks from 'eslint-plugin-react-hooks'
-// import reactRefresh from 'eslint-plugin-react-refresh'
-// import tseslint from 'typescript-eslint'
-// import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
 
-// export default tseslint.config([
-//   globalIgnores(['dist']),
-//   {
-//     files: ['**/*.{ts,tsx}'],
-//     extends: [
-//       js.configs.recommended,
-//       tseslint.configs.recommended,
-//       reactHooks.configs['recommended-latest'],
-//       reactRefresh.configs.vite,
-//     ],
-//     languageOptions: {
-//       ecmaVersion: 2020,
-//       globals: globals.browser,
-//     },
-//   },
-// ])
-
-
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
-import stylistic from '@stylistic/eslint-plugin';
-import eslintImport from 'eslint-plugin-import';
-
-export default tseslint.config(
-  { ignores: ['dist'] },
-
+export default tseslint.config([
+  globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      globals: globals.browser,
-    },
-
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'jsx-a11y': jsxA11y,
-      import: eslintImport,
-      '@stylistic': stylistic,
-    },
-
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
-      'plugin:react-hooks/recommended',
-      'plugin:jsx-a11y/recommended',
-      'plugin:import/recommended',
-      'plugin:import/typescript',
-      'plugin:prettier/recommended',
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
     ],
-
-    rules: {
-      // React Refresh
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      //prettier rule
-      'prettier/prettier': 'warn',
-      // React Hooks
-      ...reactHooks.configs.recommended.rules,
-
-      // Import
-      'import/order': [
-        'warn',
-        {
-          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-          'newlines-between': 'always',
-        },
-      ],
-      'import/no-unresolved': 'error',
-
-      // Stylistic formatting (same as Prettier, but ESLint-native)
-      '@stylistic/indent': ['error', 2],
-      '@stylistic/semi': ['error', 'always'],
-      '@stylistic/quotes': ['error', 'single'],
-      '@stylistic/comma-dangle': ['error', 'always-multiline'],
-      '@stylistic/object-curly-spacing': ['error', 'always'],
-      '@stylistic/arrow-spacing': ['error', { before: true, after: true }],
-
-      // TypeScript
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
   },
-);
+])
