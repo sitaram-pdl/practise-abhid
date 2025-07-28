c729e2e8eb42ed7ade65801313d2c2f2923db4e2
abidh_here <merosagramatha047@gmail.com>
2025-07-28 14:49:29 +0545

    feat: main app file updated to render Login component

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,11 +1,14 @@
 
 
-import { Button } from "@/components/ui/button"
+
+import Login from '../src/features/Login/Login'
 
 function App() {
+
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+
+    <div>
+      <Login />
     </div>
   )
 }
