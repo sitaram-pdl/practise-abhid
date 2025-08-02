ce960282052797bae6ffe39f34ee92163c549cfe
meroSagarmatha <merosagarmatha047@gmail.com>
2025-08-02 18:32:40 +0545

    feat:Adds register and handleSubmit props of useForm custom hook into code

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,9 +1,20 @@
 
-import { useForm } from "react-hook-form"
+import { useForm, type FieldValues  } from "react-hook-form"
 
+type FormFields = {
+    Username: string;
+    Password: string;
 
+}
 
 function Login() {
+
+    const {register,handleSubmit} = useForm<FormFields>()
+
+
+    const onSubmit = (data:FieldValues) =>{
+        console.log(data)
+    }
   
   return (
     <div className="flex ">
@@ -12,42 +23,44 @@ function Login() {
         </div> 
         <div className = "bg-grey-100 w-3/5  flex justify-center items-center" >
 
-            <div className = "bg-white w-5/10 flex flex-col px-5 py-5 rounded-md shadow-[1px_1px_8px_rgb(0,0,0,0.3)]" >
-                 
-                <div className = "text-2xl font-semibold mx-1 mb-5" >Login</div>
-                <div className = "flex flex-col mx-1 mb-3">
-                    <label className = " text-gray-700  text-base font-normal"
-                    htmlFor="zUserName">Username </label>
-                    <input 
-                        className = "border-1 px-3 py-2 rounded  "
-                        type = "text" 
-                        id = "zUserName"
-                        placeholder = "Enter Username"
-                    />
+            <form onSubmit={handleSubmit(onSubmit)} 
+                className = "bg-white w-5/10 flex flex-col px-5 py-5 rounded-md shadow-[1px_1px_8px_rgb(0,0,0,0.3)]"  >
+                    <div className = "text-2xl font-semibold mx-1 mb-5" >Login</div>
+                    <div className = "flex flex-col mx-1 mb-3">
+                        <label className = " text-gray-700  text-base font-normal"
+                            htmlFor="zUserName">Username </label>
+
+                         <input 
+                            {...register("Username",{})}
+                            className = "border-1 px-3 py-2 rounded  "
+                            type = "text" 
+                            id = "zUserName"
+                            placeholder = "Enter email"
+                        />
                   
-                </div>
-
-                <div className = "flex flex-col mx-1 mb-4"> 
-                    <label htmlFor="zPassword">Password </label>
-                    <input 
-                        className = "border-1 px-3 py-2 rounded "
-                        type = "text" 
-                        id = "zPassword"
-                        placeholder = "Enter Password"            
-                    />
-                </div>
-
-                <div className = "mx-1 mb-3"> 
-                    <button className="bg-indigo-600 hover:opacity-60  w-full rounded-md text-white px-4 py-2 " 
-                        onClick={handleButtonClick}
-                    >Sign In</button>
-                </div>
+                    </div>
+                    <div className = "flex flex-col mx-1 mb-4"> 
+                        <label htmlFor="zPassword">Password </label>
+                        <input 
+                            {...register("Password",{})}
+                            className = "border-1 px-3 py-2 rounded "
+                            type = "text" 
+                            id = "zPassword"
+                            placeholder = "Enter Password"            
+                        />
+                    </div>
+                    <div className = "mx-1 mb-3"> 
+                        <button className="bg-indigo-600 hover:opacity-60  w-full rounded-md text-white px-4 py-2 " 
+                        
+                        >Sign In</button>
+                    </div>
+                
 
                 <div className = " flex flex-col text-sm text-blue-700 mx-1 items-center leading-4">
                     <p className="text-gray-500">Demo credentials:</p>
                     <p>username: johnd | password: m38rmF$ </p>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
   )
