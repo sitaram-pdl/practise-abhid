5af0c42b3c87755e890042198469482dac9398c4
meroSagarmatha <merosagarmatha047@gmail.com>
2025-08-11 05:47:56 +0545

    feat: fetch and display data from product file

--- src/features/dashboard/products/Product.tsx
+++ src/features/dashboard/products/Product.tsx
@@ -1,26 +1,95 @@
 
-
-
 import { useEffect, useState } from "react";
-import axios from "../../../api/Api"; // now , here we use axios that was previously created.
+import axios from "@/api/Api"; // now , here we use axios that was previously created.
+import Sidebar from "@/features/dashboard/products/Sidebar";
+import Headbar from "@/features/dashboard/products/Headbar";
+import ProductTable from "@/features/dashboard/products/ProductTable"
+
+export interface RatingType {  
+  rate: number;
+  count: number;
+}
+
+export interface ProductType {
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  rating: RatingType;
+  title: string;
+  quantity?: number; // manually adding extra quantity property for easy manupulations.
+}
 
 export default function Products() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    axios.get("/products") // here, merge happens = baseURL + /products → full URL
-      .then((response) => setProducts(response.data))
+  const [products, setProducts] = useState<ProductType[]>([]);
+ 
+  
+ useEffect(() => {
+    axios
+      .get("/products")
+      .then((response) =>
+        setProducts(
+          response.data.map((eachProduct: ProductType) => ({
+            ...eachProduct,
+            quantity: 0, // here, we manually add quantity at the end of product, so every products starts with quantity 0.
+          }))
+        )
+      )
       .catch((error) => console.error("Error fetching products:", error));
   }, []);
 
+
+  console.log(products) // see the product to know about the details about it and  further analysis
+
+
+    // here we calculate total cart items for using in Headbar.
+      const totalCartItems = products.reduce(
+        (sum, product) => sum + (product.quantity || 0),
+          0
+        );
+  
+     const increaseQuantity = (productId: number) => {
+          setProducts((prev) =>
+            prev.map((eachProduct) => eachProduct.id === productId 
+              ? { ...eachProduct, quantity: (eachProduct.quantity || 0) + 1 } 
+              : eachProduct
+            )
+          );
+        };
+
+     const decreaseQuantity = (productId: number) => {
+          setProducts((prev) =>
+            prev.map((eachProduct) => eachProduct.id === productId && (eachProduct.quantity || 0) > 0
+              ? { ...eachProduct, quantity: (eachProduct.quantity || 0) - 1 } 
+              : eachProduct
+            )
+          );
+        };
+
+  const handleRemove = (currentId:number) => {
+    setProducts((previous) => previous.filter((eachProduct) => eachProduct.id !== currentId));
+  };
+
   return (
-    <div className="p-4">
-      <h1 className="text-2xl mb-4">Products</h1>
-      <ul>
-        {products.map((p: any) => (
-          <li key={p.id}>{p.title}</li>
-        ))}
-      </ul>
+    <div className="flex min-h-screen bg-gray-100">
+      <Sidebar />
+        <main className="flex-1 p-6">
+          <Headbar Quantity={totalCartItems} />
+          <input
+            type="text"
+            placeholder="Search by name"
+            className="w-full p-2 border rounded mb-4"
+          />
+          <ProductTable
+            products={products}
+            onRemove={handleRemove}
+            onIncrease = {increaseQuantity}
+            onDecrease = {decreaseQuantity}
+          />
+        </main>
     </div>
   );
 }
+
+
---
+++ src/features/dashboard/products/ProductTable.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function ProductTable() {
+  return (
+    <div>ProductTable</div>
+  )
+}
+
