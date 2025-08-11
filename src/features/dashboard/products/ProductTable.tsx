433c072bf94e5d7cea987bfb057e51d35000390e
meroSagarmatha <merosagarmatha047@gmail.com>
2025-08-11 06:03:58 +0545

    feat: Add functionality to display table in product page

--- src/features/dashboard/products/ProductTable.tsx
+++ src/features/dashboard/products/ProductTable.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
 
-export default function ProductTable() {
-  return (
-    <div>ProductTable</div>
-  )
+import ProductRow from "./ProductRow";
+import { type ProductType } from "@/features/dashboard/products/Product"
+
+interface ProductTablePropsType {
+  products: ProductType[];
+  onRemove: (id: number) => void;
+  onIncrease: (id:number) => void;
+  onDecrease: (id:number) => void;
+  Quantity: number;
 }
 
+export default function ProductTable({ products,  onRemove, onIncrease, onDecrease, }:ProductTablePropsType) {
+  return (
+    <div className="overflow-x-auto">
+      <table className="w-full bg-white rounded shadow">
+
+          <thead className="bg-gray-200">
+            <tr className="text-left">
+              <th className="p-3">ID</th>
+              <th className="p-3">Product</th>
+              <th className="p-3">Price</th>
+              <th className="p-3">Rating</th>
+              <th className="p-3">Cart</th>
+              <th className="p-3">Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {products.map((eachProduct:any) => (
+              <ProductRow
+                key={eachProduct.id}
+                eachProduct={eachProduct}
+                onRemove={onRemove}
+                onIncrease ={onIncrease}
+                onDecrease ={onDecrease}
+                Quantity={eachProduct.quantity || 0}
+              />
+            ))}
+          </tbody>
+      </table>
+    </div>
+  );
+}
