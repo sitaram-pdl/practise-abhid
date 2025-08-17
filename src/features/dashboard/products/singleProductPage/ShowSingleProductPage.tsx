105e1dab470cbf2b08166c8fcc9577387d844cf8
meroSagarmatha <merosagarmatha047@gmail.com>
2025-08-17 19:49:28 +0545

    feat: Add new file to display information about single product.

---
+++ src/features/dashboard/products/singleProductPage/ShowSingleProductPage.tsx
@@ -0,0 +1,28 @@
+
+import { useParams } from 'react-router-dom';
+import { useProductContext } from "@/context/ProductContext";
+
+
+export default function ShowSingleProductPage() {
+
+    const { productID } = useParams();
+    const { products } = useProductContext();
+  
+  const SingleProduct = products.find(
+  (eachProduct) => eachProduct.id === Number(productID) // convert string to number
+);
+
+  if (!SingleProduct) {
+    return <div className='text-2xl text-red-500 font-bold'>Product not found.</div>;
+  }
+
+
+  return (
+
+     <div className="flex flex-col h-auto absolute left-72 right-2 top-20  
+    shadow-[2px_2px_5px_1px_rgba(0,0,0,0.5)]">
+
+ 
+    </div>
+  )
+}
