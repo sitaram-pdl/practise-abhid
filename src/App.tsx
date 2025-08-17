8433f3da15f95cd2804e74b4d22d8f1d555fd6a3
meroSagarmatha <merosagarmatha047@gmail.com>
2025-08-17 19:44:36 +0545

    feat:make adjustment to handle nested routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Login from "./features/login/Login";
 import Products from "./features/dashboard/products/Products";
 import Cart from "./features/dashboard/cart/Cart";
 import User from "./features/dashboard/user/User";
+import ShowSingleProductPage from "@/features/dashboard/products/singleProductPage/ShowSingleProductPage"
 import ProtectedLayout from "./routing/privateRoute";
 import Layout from "@/routing/Layout";
 import { ProductProvider } from "@/context/ProductContext";
@@ -17,6 +18,7 @@ function App() {
           <Route element={<ProtectedLayout />}>
             <Route element={<Layout />}>
               <Route path="/products" element={<Products />} />
+              <Route path="/products/:productID" element={<ShowSingleProductPage />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/user" element={<User />} />
             </Route>
