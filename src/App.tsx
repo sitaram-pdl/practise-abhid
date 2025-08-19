9384a638cf29912881d1bf9cb884569657268736
meroSagarmatha <merosagarmatha047@gmail.com>
2025-08-19 15:43:31 +0545

    feat: update the App file to handle user context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./features/login/Login";
+
 import Products from "./features/dashboard/products/Products";
 import Cart from "./features/dashboard/cart/Cart";
 import User from "./features/dashboard/user/User";
@@ -9,21 +10,25 @@ import ProtectedLayout from "./routing/privateRoute";
 import Layout from "@/routing/Layout";
 import { ProductProvider } from "@/context/ProductContext";
 
+import {UserProvider} from "@/context/UserContext"
+
 function App() {
   return (
     <BrowserRouter>
       <ProductProvider>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route element={<ProtectedLayout />}>
-            <Route element={<Layout />}>
-              <Route path="/products" element={<Products />} />
-              <Route path="/products/:productID" element={<ShowSingleProductPage />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/user" element={<User />} />
+        <UserProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route element={<ProtectedLayout />}>
+              <Route element={<Layout />}>
+                <Route path="/products" element={<Products />} />
+                <Route path="/products/:productID" element={<ShowSingleProductPage />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/user" element={<User />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
+          </Routes>
+        </UserProvider>
       </ProductProvider>
     </BrowserRouter>
   );
