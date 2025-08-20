c000092831f546778aeb57325c87a57f7ca96fa0
meroSagarmatha <merosagarmatha047@gmail.com>
2025-08-20 14:56:31 +0545

    feat: Add feature to filter row using username in searchbar

--- src/features/dashboard/user/User.tsx
+++ src/features/dashboard/user/User.tsx
@@ -1,21 +1,31 @@
 
-// import { useState } from "react";
+import { useState } from "react";
 import UserTable from "./UserTable";
+import { useUserContext } from "@/context/UserContext";
+// import {type UsersType} from "@/features/dashboard/UserTypes"
 
 
 export default function User() {
 
+  const {users} = useUserContext()
+  const [searchTerm, setSearchTerm] = useState("")
+  
+  const filteredUsers = users.filter((eachUser) => eachUser.username.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()))
+                              
 
+  console.log("The filtered users is:",filteredUsers)
 
   return (
      <div className="flex h-auto absolute left-72 right-2 top-20  
     shadow-[2px_2px_5px_1px_rgba(0,0,0,0.5)]">
    
-        <main className="flex-1 p-6 bg-white rounded ">
+        <main className="flex-1 p-6 w-full bg-white rounded ">
             <div className="flex justify-between items-center border-t py-4 pr-2 ">
                 <input
                   type="text"
-                  placeholder="Search by name"
+                  placeholder="Search by username"
+                  value={searchTerm}
+                  onChange={(e)=>setSearchTerm(e.target.value)}
                   className="w-1/3 p-2 border rounded"
                 />
                 <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700"
@@ -24,8 +34,8 @@ export default function User() {
                   Add New Product
                 </button>
             </div>
-            <div>
-                <UserTable />
+            <div className="w-full">
+                <UserTable filteredUsers={filteredUsers}  />
             </div>
         </main>
 
