70c347e254911edc148b2b0de1f6d0d89d616e7d
meroSagarmatha <merosagarmatha047@gmail.com>
2025-08-20 09:39:50 +0545

    feat: add functionality and css to display data efficiently

--- src/features/dashboard/user/User.tsx
+++ src/features/dashboard/user/User.tsx
@@ -1,12 +1,34 @@
 
+// import { useState } from "react";
+import UserTable from "./UserTable";
 
 
 export default function User() {
 
+
+
   return (
-     <div className="flex min-h-screen absolute left-72 right-2 top-20  
+     <div className="flex h-auto absolute left-72 right-2 top-20  
     shadow-[2px_2px_5px_1px_rgba(0,0,0,0.5)]">
-        <h1>This page will be utilized to work for the handling user page.</h1>
+   
+        <main className="flex-1 p-6 bg-white rounded ">
+            <div className="flex justify-between items-center border-t py-4 pr-2 ">
+                <input
+                  type="text"
+                  placeholder="Search by name"
+                  className="w-1/3 p-2 border rounded"
+                />
+                <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700"
+                  // onClick={handleAddNewProduct}
+                  >
+                  Add New Product
+                </button>
+            </div>
+            <div>
+                <UserTable />
+            </div>
+        </main>
+
     </div>
   )
 }
