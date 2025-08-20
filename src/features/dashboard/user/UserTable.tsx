91ca84fc4341283dfbff14202a4b08703e6fcda4
meroSagarmatha <merosagarmatha047@gmail.com>
2025-08-20 09:37:46 +0545

    feat: create page and handle table and row related issues

---
+++ src/features/dashboard/user/UserTable.tsx
@@ -0,0 +1,30 @@
+
+
+
+
+
+export default function UserTable() {
+
+
+
+  return (
+
+    <div>
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Actions</th>
+            </tr>
+            
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+  )
+}
