51a15e881e1a154863239146e428b4f1426b0f0d
meroSagarmatha <merosagarmatha047@gmail.com>
2025-08-20 15:00:06 +0545

    Refactor: take user data as props instead of using from context.

--- src/features/dashboard/user/UserTable.tsx
+++ src/features/dashboard/user/UserTable.tsx
@@ -1,30 +1,32 @@
 
+import UserRow from "./UserRow"
+import {type UsersType} from "@/features/dashboard/UserTypes"
 
+interface UserTablePropsType {
+  filteredUsers: UsersType[];
+}
 
+export default function UserTable({filteredUsers}:UserTablePropsType) {
 
-
-export default function UserTable() {
-
-
-
-  return (
-
-    <div>
-        <table>
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Actions</th>
-            </tr>
-            
+    return (
+        <table className="w-full table-auto"> {/* Add table-auto for proper sizing */}
+            <thead className="w-full bg-gray-500 text-lg text-white">
+                <tr className="w-full">
+                    <th className="px-4 py-2 text-left">ID</th>
+                    <th className="px-4 py-2 text-left">Username</th>
+                    <th className="px-4 py-2 text-left">Email</th>
+                    <th className="px-4 py-2 text-left">Password</th>
+                    <th className="px-4 py-2 text-left">Actions</th>
+                </tr>
             </thead>
-            <tbody>
-
+            <tbody className="w-full">
+                {filteredUsers.map((eachUser) => (
+                    <UserRow 
+                        key={eachUser.id}
+                        eachUser={eachUser}
+                    />
+                ))}
             </tbody>
         </table>
-    </div>
-  )
+    )
 }
