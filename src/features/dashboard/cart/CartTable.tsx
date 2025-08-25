2ef8890643a23903238eb1b9494082957be8f38c
meroSagarmatha <merosagarmatha047@gmail.com>
2025-08-25 12:33:19 +0545

    feat: Create Cart table to render the propertis of api call

---
+++ src/features/dashboard/cart/CartTable.tsx
@@ -0,0 +1,35 @@
+
+
+import { useCartContext } from "@/context/CartContext";
+import CartRow from "./CartRow";
+
+
+export default function CartTable() {
+
+    const {carts} = useCartContext()
+
+
+  return (
+
+    <table className="w-full table-auto border">
+        <thead className="bg-gray-500 text-white text-lg">
+            <tr className="w-full">
+                <th className="px-4 py-2 text-left">Card ID</th>
+                <th className="px-4 py-2 text-left">Date</th>
+                <th className="px-4 py-2 text-left">Total Quantity</th>
+                <th className="px-4 py-2 text-left">Products</th>
+                <th  className="px-4 py-2 text-left">Actions</th>
+            </tr>
+        </thead>
+        <tbody className="w-full">
+            {carts.map((eachItem)=> 
+                <CartRow 
+                    key={eachItem.id}
+                    eachItem = {eachItem}
+                />
+            )}
+            
+        </tbody>
+    </table>
+  )
+}
