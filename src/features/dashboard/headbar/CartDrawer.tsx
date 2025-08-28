f6b3417eb4e8639cc219df9a028dd96a4e91e77a
meroSagarmatha <merosagarmatha047@gmail.com>
2025-08-28 12:58:27 +0545

    feat: Add extra features in cartDrawer page.

--- src/features/dashboard/headbar/CartDrawer.tsx
+++ src/features/dashboard/headbar/CartDrawer.tsx
@@ -1,14 +1,13 @@
 
 import { FaTrash, FaMinus, FaPlus } from "react-icons/fa";
 import { useProductContext } from "@/context/ProductContext";
-
+import Products from "../products/Products";
 
 export default function CartDrawer() {
 
   const { cartQuantity, selectedProducts, isCartOpen, setCartOpen, increaseCartQuantity, decreaseCartQuantity, removeCartItem, clearCart, totalPrice} = useProductContext();
 
   return (
-
     <div>
         {/* Overlay................................. */}
         {/* if isCartOpen state is true open the cart. and onclick just close the cart */}
@@ -20,7 +19,7 @@ export default function CartDrawer() {
         )}
         {/* Drawer .....................................*/}
         <div
-            className={`fixed right-0 top-0  h-full w-150 bg-white shadow-lg z-50 transform transition-transform duration-300 ${
+            className={`fixed right-0 top-0  h-full w-250 bg-white shadow-lg z-50 transform transition-transform duration-300 ${
               isCartOpen ? "translate-x-0" : "translate-x-full"
             }`}
           >
@@ -37,50 +36,57 @@ export default function CartDrawer() {
               </div>
 
               {/* Cart items...................................... */}
-              <div className="p-4 flex flex-col gap-4 overflow-y-auto h-[calc(100%-150px)]">
-                    {selectedProducts.length === 0 && (
-                      <p className="text-gray-500">Your cart is empty.</p>
-                    )}
+              <div className="flex justify-between align-center gap-2 w-full">
+                  <div className=" absolute  p-4 flex flex-col gap-4 overflow-y-auto w-2/5
+                   h-[calc(100%-9rem)]">
+                      {selectedProducts.length === 0 && (
+                        <p className="text-gray-500">Your cart is empty.</p>
+                      )}
 
-                    {selectedProducts.map((item) => (
-                      // this below div is for single horizontal product.
-                      <div key={item.id} className="flex items-center gap-3 border-b pb-3">
-                          <img
-                            src={item.image}
-                            alt={item.title}
-                            className="w-12 h-12 object-cover"
-                          />
+                      {selectedProducts.map((item) => (
+                        // this below div is for single horizontal product.
+                        <div key={item.id} className="flex items-center gap-3 border-b pb-3">
+                            <img
+                              src={item.image}
+                              alt={item.title}
+                              className="w-12 h-12 object-cover"
+                            />
 
-                          <div className="w-1/2 overflow-y-auto">
-                            <div className="font-medium truncate">{item.title}</div>
-                            <div className="text-sm text-gray-500">{item.category}</div>
-                            <div className="text-sm font-semibold">${item.price}</div>
-                          </div>
+                            <div className="w-1/2 overflow-y-auto">
+                              <div className="font-medium truncate">{item.title}</div>
+                              <div className="text-sm text-gray-500">{item.category}</div>
+                              <div className="text-sm font-semibold">${item.price}</div>
+                            </div>
 
-                          {/* Quantity Controls.............................................. */}
-                          <div className="flex items-center border px-2 py-1 rounded">
-                              <button onClick={() => decreaseCartQuantity(item.id)}>
-                                <FaMinus />
-                              </button>
-                              <span className="px-2">{cartQuantity[item.id]}</span>
-                              <button onClick={() => increaseCartQuantity(item.id)}>
-                                <FaPlus />
-                              </button>
-                          </div>
+                            {/* Quantity Controls.............................................. */}
+                            <div className="flex items-center border px-2 py-1 rounded">
+                                <button onClick={() => decreaseCartQuantity(item.id)}>
+                                  <FaMinus />
+                                </button>
+                                <span className="px-2">{cartQuantity[item.id]}</span>
+                                <button onClick={() => increaseCartQuantity(item.id)}>
+                                  <FaPlus />
+                                </button>
+                            </div>
 
-                          <button
-                                onClick={() => removeCartItem(item.id)}
-                                className="text-red-500 hover:text-red-700 hover:cursor-pointer ml-5"
-                              >
-                                <FaTrash />
-                          </button>
-                      </div>
-                    ))}
+                            <button
+                                  onClick={() => removeCartItem(item.id)}
+                                  className="text-red-500 hover:text-red-700 hover:cursor-pointer ml-5"
+                                >
+                                  <FaTrash />
+                            </button>
+                        </div>
+                      ))}
+                  </div>
+                  <div className=" absolute right-2 p-2 flex flex-col gap-2 overflow-y-auto w-3/5
+                   h-[calc(100%-9rem)] border-l ">
+                    <Products isInsideCart={true} />
+                  </div>
               </div>
               {/* Footer.............................................................. */}
-              <div className="p-4 border-t">
+                <div className="fixed bottom-1 right-0 left-0 p-4 border-t">
               
-                  <div className="flex justify-between items-center gap-2">
+                  <div className=" flex justify-between items-center gap-2 z-10 ">
                       <button
                           className="w-fit px-4 py-2 bg-red-600 text-white  rounded hover:bg-red-700"
                           onClick={clearCart}
@@ -88,7 +94,7 @@ export default function CartDrawer() {
                           <FaTrash className="inline mr-1" /> Clear Cart
                       </button>
 
-                      <div className="flex justify-between font-bold gap-3 ">
+                      <div className="flex justify-between  text-lg font-bold gap-3 ">
                           <span>Total: </span>
                           <span>${ totalPrice.toFixed(2)}</span>
                       </div>
@@ -102,6 +108,5 @@ export default function CartDrawer() {
     </div>
   );
 }
-
 
 
