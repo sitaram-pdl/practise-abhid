fabe97b614828676d7091001e8c431734c29c51a
meroSagarmatha <merosagarmatha047@gmail.com>
2025-09-01 10:10:11 +0545

    feat: Add icon on notification component

--- src/features/dashboard/cart/Notification.tsx
+++ src/features/dashboard/cart/Notification.tsx
@@ -1,6 +1,8 @@
 
 import { useEffect, useState } from "react";
 import { useCartContext } from "@/context/CartContext";
+import {FaCheckCircle } from "react-icons/fa"
+
 
 export default function Notification() {
 
@@ -18,7 +20,7 @@ export default function Notification() {
         setIsVisible(false); // trigger fade out..
         // Delay message clearing to allow fade-out animation
         setTimeout(() => setNotificationMessage(""), 300); // remove from DOM after fade out
-      }, 3000);
+      }, 2000);
 
       return () => clearTimeout(hideTimer);
     }
@@ -31,9 +33,10 @@ export default function Notification() {
       className={`fixed top-4 right-4 bg-green-500 text-white px-4 py-2 rounded shadow-lg z-[1000]
       transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`}
     >
-      <div className="text-lg font-bold text-white">
-        {currentMessage}
-      </div>
+       <div className="flex items-center gap-2 text-lg font-bold text-white">
+          <FaCheckCircle className="h-5 w-5 text-white" />
+          {currentMessage}
+        </div>
     </div>
   );
 }
