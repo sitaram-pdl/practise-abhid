5fb09724b45e58fc363cef247d50d05c67392f0b
meroSagarmatha <merosagarmatha047@gmail.com>
2025-09-01 10:12:55 +0545

    feat: Add the types to all the handlers used for cart section

--- src/features/dashboard/CartTypes.ts
+++ src/features/dashboard/CartTypes.ts
@@ -1,5 +1,8 @@
 
+import type { ProductType } from "./types";
 
+
+// ....................................................
 export interface CartProduct {
   productId: number;
   quantity: number;
@@ -7,11 +10,28 @@ export interface CartProduct {
 
 export interface CartTypes{
     date: string;
-    id: number;
+    id: number
     products: CartProduct[];
-    userId: number;
+    userId: number
     __v : number;
 }
+// ..........................................
+
+export interface CartProductDetails {
+  productDetails: ProductType;
+  quantity: number;
+}
+
+export interface CartWithProductDetailsType{
+    date: string;
+    id?: number;
+    products: CartProductDetails[];
+    userId: number;
+    __v : number;
+
+}
+
+// ...............................................................
 
 export interface CartContextType{
     carts: CartTypes[];
@@ -19,17 +39,25 @@ export interface CartContextType{
     deleteTargetId: number | null;
     notificationMessage: string;
     singleCartData: CartTypes;
+    isUpdateCart:boolean;
+    isLoading:boolean;
 
     setDeleteCartModalOpen: (isOpen:boolean) => void;
     setDeleteTargetId: (id:number | null) => void;
     setNotificationMessage: (message:string) => void;
     setSingleCartData: (data:any) => void;
+    setUpdateCart: (isUpdate:boolean) => void;
 
+    hydratedSingleCartData:CartWithProductDetailsType| null;
+    loadHydratedSingleCartData: () => void;
     handleDeleteCart:(id:number) => void;
     confirmDelete: () => Promise<void>;
     fetchSingleCartData: (id:number) => Promise<void>
+    handleUpdateCart:() => void;
+    ConfirmUpdateCart: (id:number ,editedCart:CartWithProductDetailsType) => void;
 }
 
+// ....................................................................
 export interface ProviderPropsType{
     children: React.ReactNode;
 }
