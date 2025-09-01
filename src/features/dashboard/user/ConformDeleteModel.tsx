d873be8e6bce57e3f7fa048bd92e702adca491d5
meroSagarmatha <merosagarmatha047@gmail.com>
2025-09-01 10:18:04 +0545

    style: chage the css code of delete model

--- src/features/dashboard/user/ConformDeleteModel.tsx
+++ src/features/dashboard/user/ConformDeleteModel.tsx
@@ -6,16 +6,10 @@ export default function ConfirmDeleteModal() {
   
   const { isDeleteModalOpen, setDeleteModalOpen, confirmDelete } = useUserContext();
 
- 
   if (!isDeleteModalOpen) return null;
 
   return (
     <>
-      {/* Overlay with fade effect */}
-      {/* <div 
-        className="fixed inset-0  z-50 backdrop-brightness-60"
-        onClick={() => setDeleteModalOpen(false)}
-      /> */}
       { isDeleteModalOpen && (
           <div
             className="fixed inset-0 z-50 backdrop-brightness-40 "
@@ -25,13 +19,13 @@ export default function ConfirmDeleteModal() {
 
       {/* Modal positioned fixed at center */}
       <div className=" fixed inset-0 flex flex-1 items-center justify-center z-50 ">
-        <div className="absolute bg-white rounded-lg shadow-xl p-6 w-96 mx-4">
-          <h2 className="text-lg font-bold mb-4">Confirm Deletion</h2>
-          <p className="mb-6">Are you sure you want to delete this product?</p>
+      <div className="absolute bg-white rounded-lg shadow-xl p-6 w-105 mx-4">
+          <h2 className="text-xl font-bold mb-4 text-center text-red-500">Confirm Deletion</h2>
+          <p className="mb-6 text-lg text-center">Are you sure you want to delete this User ?</p>
 
-          <div className="flex justify-end gap-3">
+          <div className="flex justify-between gap-3">
             <button
-              className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 transition-colors"
+              className="px-4 py-2 text-white bg-gray-400 rounded hover:bg-gray-600 transition-colors"
               onClick={() => setDeleteModalOpen(false)}
             >
               Cancel
