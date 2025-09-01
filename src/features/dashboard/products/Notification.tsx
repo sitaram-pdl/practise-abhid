59743323a2051a4c3e15f00935e008f3c25e9f16
meroSagarmatha <merosagarmatha047@gmail.com>
2025-09-01 10:11:04 +0545

    feat:Add the icon on notification component

--- src/features/dashboard/products/Notification.tsx
+++ src/features/dashboard/products/Notification.tsx
@@ -1,6 +1,9 @@
 
 import { useEffect, useState } from "react";
 import { useProductContext } from "@/context/ProductContext";
+import {FaCheckCircle } from "react-icons/fa"
+
+
 
 export default function Notification() {
   const { notificationMessage, setNotificationMessage } = useProductContext();
@@ -29,8 +32,9 @@ export default function Notification() {
       className={`fixed top-4 right-4 bg-green-500 text-white px-4 py-2 rounded shadow-lg z-[1000]
       transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`}
     >
-      <div className="text-lg font-bold text-white">
-        {currentMessage}
+       <div className="flex items-center gap-2 text-lg font-bold text-white">
+             <FaCheckCircle className="h-5 w-5 text-white" />
+             {currentMessage}
       </div>
     </div>
   );
