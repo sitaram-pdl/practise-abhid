c8b1bcbd7ddb0721e4715a31e24b848bbce23f5f
meroSagarmatha <merosagarmatha047@gmail.com>
2025-09-02 12:42:55 +0545

    feat: Add necessaray styles and functionality for header

--- src/features/dashboard/headbar/HeadBar.tsx
+++ src/features/dashboard/headbar/HeadBar.tsx
@@ -1,14 +1,15 @@
 
 
-import { FaShoppingCart } from "react-icons/fa";
+import { ShoppingCart } from 'lucide-react';
 import { useNavigate, useLocation } from "react-router-dom";
+
 import { useProductContext } from '@/context/ProductContext';
 import CartDrawer from "./CartDrawer";
 
 const RouteTitles: Record<string, string> = {
   '/products': 'Available Products',
   '/cart': 'Shopping Cart',
-  '/user': 'User Profile',
+  '/users': 'User Profile',
 };
 
 
@@ -32,34 +33,36 @@ export default function HeadBar() {
 
   return (
     <>
-      <div className="fixed top-0 left-69 right-0 h-16 z-10 px-4 py-4 bg-amber-300 flex justify-between items-center ">
-        <h2 className="text-2xl font-semibold">{getPageTitle()}</h2>
+      <div className="fixed top-0 left-69 right-0 h-17 z-10 px-4 py-4  bg-neutral-100 flex justify-between items-center border ">
+        <h2 className="pl-2 text-3xl  text-blue-900  font-bold  
+           drop-shadow-[0.1rem_0.1rem_0rem_#555] ">
+            {getPageTitle()}</h2>
         <div className="flex items-center gap-6">
         
-        <div className="relative cursor-pointer"
-          onClick={() => setCartOpen(true)}
-          >
-            <FaShoppingCart className="text-2xl" />
-            {totalCartsQuantity > 0 
-              ? (
-                  <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-1">
-                    {totalCartsQuantity}
-                  </span>
-                )
-              : ( <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-1">
-                    {0}
-                  </span> )
-          }
-          </div>
-
-          <div className="w-10 h-10 rounded-full bg-gray-300 flex items-center justify-center">
-            JO
-          </div>
-          <button  className="ml-auto bg-red-600 px-2 py-1 rounded text-white"
-            onClick={logout}
+          <div className="relative cursor-pointer"
+            onClick={() => setCartOpen(true)}
             >
-            Logout
-          </button>
+              <ShoppingCart className="text-2xl text-blue-700" />
+              {totalCartsQuantity > 0 
+                ? (
+                    <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-1">
+                      {totalCartsQuantity}
+                    </span>
+                  )
+                : ( <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-1">
+                      {0}
+                    </span> )
+            }
+            </div>
+
+            <div className="w-10 h-10 text-blue-700 rounded-full bg-gray-200 flex items-center justify-center">
+              JO
+            </div>
+            <button  className="ml-auto bg-red-400 hover:bg-red-500 px-2 py-1 rounded text-bold text-white"
+              onClick={logout}
+              >
+              Logout
+            </button>
         </div>
       </div>
        {/* call  CartDrawer component from moved the header */}
